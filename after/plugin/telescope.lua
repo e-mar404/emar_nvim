88354317d7109d0a8cacc7de654f24dc088e9c19
emar <egmarinn@gmail.com>
2024-09-18 16:38:17 -0500

    Move telescope to the after plugins

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,4 +4,5 @@ local Map = vim.keymap.set
 Map('n', '<leader>pf', builtin.find_files, {})
 Map('n', '<leader>pg', builtin.git_files, {})
 Map('n', '<leader>lg', builtin.live_grep, {})
+Map('n', '<leader>fb', builtin.buffers, {})
 Map('n', '<leader>th', ':Telescope help_tags<Enter>')
