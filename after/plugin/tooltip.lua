bc55820d86deb3247f94f1a17226d23b8671c9c1
emar <egmarinn@gmail.com>
2024-09-18 16:41:36 -0500

    add languages

--- after/plugin/tooltip.lua
+++ after/plugin/tooltip.lua
@@ -5,6 +5,8 @@ require('tooltip').setup({
     ['.go'] = 'go run %s',
     ['.erl'] = 'escript %s',
     ['.scala'] = 'scala %s',
+    ['.clj'] = 'clojure -M %s',
+    ['.lua'] = 'lua %s',
   },
   styled = true,
 })
